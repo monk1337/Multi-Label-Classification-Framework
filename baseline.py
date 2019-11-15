5f84ce1a72cfc4bf1ac0c884a7646c27f933a97f
Aaditya Ura <3x3paul@gmail.com>
2019-11-15 12:53:49 +0530

    Create baseline.py

---
+++ baseline.py
@@ -0,0 +1,68 @@
+import numpy as np
+from skmultilearn.adapt import MLkNN
+from scipy.sparse import csr_matrix, lil_matrix
+from skmultilearn.problem_transform import LabelPowerset
+from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.naive_bayes import GaussianNB
+
+class Base_models(object):
+    
+    def __init__(self, x_data, y_data, x_test, y_test):
+        
+        self.x_data = x_data
+        self.y_data = y_data
+        self.x_test = x_test
+        self.y_test = y_test
+        
+    def BinaryRe(self):
+        classifier = BinaryRelevance(GaussianNB())
+        classifier.fit(self.x_data,  self.y_data)
+        predictions = classifier.predict(self.x_test)
+
+        y_test      = np.array(self.y_test,dtype = np.float32)
+        predictions = np.array(predictions,dtype = np.float32)
+
+        return {
+                 'accuracy': accuracy_score(y_test,predictions), 
+                'f1_score': f1_score(y_test, predictions, average='micro') 
+               }
+    
+    
+    def powerset(self):
+        
+        classifier = LabelPowerset(LogisticRegression())
+        classifier.fit(self.x_data,  self.y_data)
+        y_test      = np.array(self.y_test,dtype = np.float32)
+        predictions = np.array(predictions,dtype = np.float32)
+
+        predictions = classifier.predict(self.x_test)
+        
+        y_test      = np.array(self.y_test,dtype = np.float32)
+        predictions = np.array(predictions,dtype = np.float32)
+
+        return {
+                 'accuracy': accuracy_score(y_test,predictions), 
+                'f1_score': f1_score(y_test, predictions, average='micro') 
+               }
+    
+    def mlknn(self):
+        
+        classifier_new = MLkNN(k=10)
+
+        x_train = lil_matrix(self.x_data).toarray()
+        y_train = lil_matrix(self.y_data).toarray()
+        x_test = lil_matrix(self.x_test).toarray()
+
+        classifier_new.fit(x_train, y_train)
+
+        # predict
+        predictions = classifier_new.predict(x_test)
+        y_test      = np.array(self.y_test,dtype = np.float32)
+        
+        return {
+                 'accuracy': accuracy_score(y_test,predictions), 
+                'f1_score': f1_score(y_test, predictions, average='micro') 
+               }
+    
+    
+        
