19e1bf636c0a25526d044f95c3c9442ce3f14c09
Aaditya Ura <3x3paul@gmail.com>
2019-11-17 03:40:42 +0530

    Update data.py

--- data.py
+++ data.py
@@ -35,184 +35,6 @@
 # # Sparse terms – We are often not interested in infrequent terms in our documents. Such “sparse” terms should be removed from the document term matrix.
 
 
-
-# def lower_case(sentences):
-#     return [sentence.lower() for sentence in sentences]
-
-# def keep(sentences,labels,ratio):
-#     data_size = int(len(sentences) * ratio)
-#     sentences = sentences[:data_size]
-#     labels    = labels[:data_size]
-#     return sentences,labels
-
-# def split_data(sentences,labels):
-#     X_train, X_val, y_train, y_val = train_test_split(sentences, labels, train_size=0.8)
-#     return X_train, X_val, y_train, y_val
-
-
-# def cleanHtml(sentence):
-#     cleanr = re.compile('<.*?>')
-#     cleantext = re.sub(cleanr, ' ', str(sentence))
-#     return cleantext
-
-
-# def cleanPunc(sentence): #function to clean the word of any punctuation or special characters
-#     cleaned = re.sub(r'[?|!|\'|"|#]',r'',sentence)
-#     cleaned = re.sub(r'[.|,|)|(|\|/]',r' ',cleaned)
-#     cleaned = cleaned.strip()
-#     cleaned = cleaned.replace("\n"," ")
-#     return cleaned
-
-
-# def keepAlpha(sentence):
-#     alpha_sent = ""
-#     for word in sentence.split():
-#         alpha_word = re.sub('[^a-z A-Z]+', ' ', word)
-#         alpha_sent += alpha_word
-#         alpha_sent += " "
-#     alpha_sent = alpha_sent.strip()
-#     return alpha_sent
-
-
-
-# stop_words = set(stopwords.words('english'))
-# re_stop_words = re.compile(r"\b(" + "|".join(stop_words) + ")\\W", re.I)
-# def removeStopWords(sentence):
-#     global re_stop_words
-#     return re_stop_words.sub(" ", sentence)
-
-
-
-# stemmer = SnowballStemmer("english")
-# def stemming(sentence):
-#     stemSentence = ""
-#     for word in sentence.split():
-#         stem = stemmer.stem(word)
-#         stemSentence += stem
-#         stemSentence += " "
-#     stemSentence = stemSentence.strip()
-#     return stemSentence
-
-
-# def tf_idf(train_sentences,test_sentences):
-    
-#     #error while passing list 
-#     #solution : converting into pandas frame
-
-#     pd_train_frame = pd.DataFrame({'text': train_sentences})
-#     pd_test_frame =  pd.DataFrame({'text': test_sentences})
-#     pd_train_frame = pd_train_frame['text']
-#     pd_test_frame  = pd_test_frame['text']
-    
-#     vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,3), norm='l2')
-#     vectorizer.fit(pd_train_frame)
-#     vectorizer.fit(pd_test_frame)
-#     x_train = vectorizer.transform(pd_train_frame)
-#     x_test = vectorizer.transform(pd_test_frame)
-    
-#     return x_train,x_test
-
-
-# #demo data
-# def get_keras_data():
-    
-#     with open('r_steamed_st.pkl','rb') as f:
-#         sentences = pk.load(f)
-#     with open('r_steamed_lb.pkl','rb') as f:
-#         labels = pk.load(f)
-        
-#     return lower_case(sentences), labels
-
-# def get_retu_data(keep_ratio):
-    
-#     with open('reuters_sentences_st.pkl','rb') as f:
-#         sentences = pk.load(f)
-    
-#     with open('all_raw_labels_keras.pkl','rb') as f:
-#         labels = pk.load(f)
-
-#     with open('label_order_keras.pkl','rb') as f:
-#         columns = pk.load(f)
-
-#     with open('label_encoding_keras.pkl','rb') as f:
-#         label_embedding = pk.load(f)
-
-#     with open('one_hot_encoding_keras.pkl','rb') as f:
-#         one_hot = pk.load(f)
-
-#     with open('adj_matrix_keras.pkl','rb') as f:
-#         adj_matrix = pk.load(f)
-        
-    
-        
-#     if keep_ratio:
-#         sentences, labels = keep(sentences,labels,keep_ratio)
-        
-        
-#     return lower_case(sentences),labels,columns,label_embedding,one_hot,adj_matrix
-
-# from sklearn.preprocessing import MultiLabelBinarizer
-
-
-# def labels_to_dataframe(sentences,labels):
-#     mlb              = MultiLabelBinarizer()
-#     labels_on        = mlb.fit_transform(labels)
-#     pd_data          = pd.DataFrame(labels_on)
-#     pd_data.columns  = mlb.classes_
-#     pd_data['text']  = sentences
-#     return pd_data
-
-# def one_hot_to_dataframe(sentences,labels,columns_name):
-
-#     pd_data          = pd.DataFrame(labels)
-#     pd_data.columns  = columns_name
-#     pd_data['text']  = sentences
-#     return pd_data
-
-# def dataframe_columns(df,columns_list):
-#     text_col = df['text']
-#     df = df.drop('text', 1)
-#     if len(df.columns) == len(columns_list):
-#         df = df.reindex(columns = columns_list)
-#         df['text'] = text_col
-#         return df
-#     else:
-#         return 'column list is not equal to data_frame columns'
-    
-# def adj_matrix(df):
-#     u = np.diag(np.ones(df.shape[1], dtype=bool))
-#     return df.T.dot(df) * (~u)
-
-
-# def get_raw_data():
-    
-#     documents = reuters.fileids()
-#     train_docs_id = list(filter(lambda doc: doc.startswith("train"),
-#                                 documents))
-#     test_docs_id = list(filter(lambda doc: doc.startswith("test"),
-#                                documents))
-#     X_train = [(reuters.raw(doc_id)) for doc_id in train_docs_id]
-#     X_test = [(reuters.raw(doc_id)) for doc_id in test_docs_id]
-
-
-#     mlb = MultiLabelBinarizer()
-#     y_train = [reuters.categories(doc_id)
-#                                  for doc_id in train_docs_id]
-#     y_test = [reuters.categories(doc_id)
-#                             for doc_id in test_docs_id]
-
-#     all_dataa     =    X_train +  X_test
-#     all_lavelsa   =    y_train +  y_test
-
-
-
-#     mlb = MultiLabelBinarizer()
-#     datas_y = mlb.fit_transform(all_lavelsa)
-    
-#     return all_dataa, datas_y, mlb.classes_
-
-
-
 import pickle as pk
 import sys
 import warnings
@@ -258,8 +80,8 @@ def keep(sentences,labels,ratio):
     labels    = labels[:data_size]
     return sentences,labels
 
-def split_data(sentences,labels):
-    X_train, X_val, y_train, y_val = train_test_split(sentences, labels, train_size=0.8)
+def split_data(sentences,labels, train_ration = 0.8):
+    X_train, X_val, y_train, y_val = train_test_split(sentences, labels, train_size = train_ration)
     return X_train, X_val, y_train, y_val
 
 
@@ -327,7 +149,7 @@ def stopit_dude(sentences):
 
 def tf_idf(train_sentences,test_sentences):
     
-    #error while passing list 
+    #error while passing list to tfidf vectors
     #solution : converting into pandas frame
 
     pd_train_frame = pd.DataFrame({'text': train_sentences})
@@ -341,7 +163,7 @@ def tf_idf(train_sentences,test_sentences):
     x_train = vectorizer.transform(pd_train_frame)
     x_test = vectorizer.transform(pd_test_frame)
     
-    return x_train,x_test
+    return x_train, x_test
 
 
 #demo data
@@ -435,6 +257,11 @@ def keep_labels(df, keep_ratio = False, freq_Value = False):
     
     df_          = df_[sorted(keep_columns)]
     df_['text']  = text_col
+    
+    #remove all rows where all labels are zeros
+    df_          = df_[(df_.loc[:, df_.columns != 'text'].T != 0).any()]
+    df_          = df_.reset_index(drop=True)
+
     return df_, sorted_long
 
 def adj_matrix(df):
